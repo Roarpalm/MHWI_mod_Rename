307f6216844582d307efcfafd1dd5e86ecaf2fdd
Roarpalm <307687673@qq.com>
2020-07-20 15:53:43 +0800

    添加项目文件。

---
+++ MHWI_mod_rename/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace MHWI_Rename
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("拖拽文件夹到此处：");
+            string dir = Console.ReadLine();
+            Console.WriteLine("输入防具编号（格式：000_0000）：");
+            string code = Console.ReadLine();
+            FindFile(dir, code);
+            Console.WriteLine("按任意键退出");
+            Console.ReadKey();
+        }
+        // 递归遍历文件夹和文件
+        public static void FindFile(string dirPath, string code)
+        {
+            var dir = new DirectoryInfo(dirPath);
+            try
+            {
+                foreach (DirectoryInfo d in dir.GetDirectories())
+                {
+                    string path = d.FullName;
+                    if (Check(path))
+                    {
+                        path = Tihuan(d.FullName, code);
+                        try
+                        {
+                            Directory.Move(d.FullName, path);
+                            Console.WriteLine("文件夹：" + path);
+                        }
+                        catch (Exception){}
+                    }
+                    FindFile(path, code);
+                }
+                foreach (FileInfo f in dir.GetFiles())
+                {
+                    if (Check(f.FullName))
+                    {
+                        string newpath = Tihuan(f.FullName, code);
+                        File.Move(f.FullName, newpath);
+                        Console.WriteLine("文件：" + newpath);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        // 替换编号
+        public static string Tihuan(string str, string code)
+        {
+            string pattern = "[0-9]{3}_[0-9]{4}";
+            var rgx = new Regex(pattern);
+            return rgx.Replace(str, code);
+        }
+        // 检查文件及文件夹是否包含关键字
+        public static bool Check(string str)
+        {
+            Regex reg = new Regex(@"[0-9]{3}_[0-9]{4}");
+            Match match = reg.Match(str);
+            if (match.Success) return true;
+            else return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+- C# 编写，带有一个exe
+
+![界面](img/MHWI_Rename.png?raw=true)
